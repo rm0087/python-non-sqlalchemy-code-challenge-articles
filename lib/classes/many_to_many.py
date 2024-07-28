691e221a3f9640316aafacfcbecae8fc64d93f5f
rm0087 <michetti.ray@gmail.com>
2024-07-27 21:51:24 -0400

    progress

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -8,9 +8,34 @@ class Author:
     def __init__(self, name):
         self.name = name
 
-    def articles(self):
-        pass
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and len(name) > 0 and not hasattr(self, 'name'):
+            self._name = name
+        else:
+            print("Author name must be a string with at least 1 character!")
+            
 
+    def articles(self):
+        def __init__(self, author, magazine, title):
+            self.author = author
+            self.magazine = magazine
+            self.title = title
+
+        @property
+        def author(self):
+            return self._author
+        
+        # @author.setter
+        # def author(self, author):
+        #     if isinstance(author, str) and len(author) > 0:
+        #         self._author = author
+        #         pass
+    
     def magazines(self):
         pass
 
