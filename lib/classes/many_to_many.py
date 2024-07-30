f55a13e8ec4a3f8f411b16f25f7940d4e44aa4f5
rm0087 <michetti.ray@gmail.com>
2024-07-30 17:38:01 -0400

    after-class

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,21 +1,35 @@
 class Article:
+    all = []
+    test_dict = {"author":"", 
+                 "magazine":"", 
+                 "title":""}
+    
+    authors = []
+    articles = []
+    magazines = []
+    
     def __init__(self, author, magazine, title):
         self.author = author
         self.magazine = magazine
         self.title = title
 
-    @property
+    @property ## AUTHOR ###############################################################
     def author(self):
         return self._author
     
     @author.setter
     def author(self, author):
+        global authors
         if isinstance(author, Author):
             self._author = author
+            Article.test_dict["author"] = self.author.name
+            if author.name not in Article.authors:
+                Article.authors.append(author.name)
+            else: pass
         else:
             print("Must be a valid author")
         
-    @property
+    @property ## MAGAZINE ###############################################################
     def magazine(self):
         return self._magazine
     
@@ -23,10 +37,12 @@ class Article:
     def magazine(self, magazine):
         if isinstance(magazine, Magazine):
             self._magazine = magazine
+            Article.test_dict["magazine"] = magazine.name
+
         else:
             print("Must be a valid magazine")
 
-    @property
+    @property ## TITLE ###############################################################
     def title(self):
         return self._title
     
@@ -34,11 +50,15 @@ class Article:
     def title(self, title):
         if isinstance(title, str) and 5 <= len(title) <= 50 and not hasattr(self, 'title'):
             self._title = title
+            Article.test_dict["title"] = title
+            Article.all.append(Article.test_dict.copy())
         else:
             print("Title must be a string between 5 and 50 characters")
         
         
 class Author:
+    articles_arr = []
+    
     def __init__(self, name):
         self.name = name
 
@@ -55,25 +75,30 @@ class Author:
             
 
     def articles(self):
+        for articles in Article.all:
+                if articles["author"] == self.name:
+                    self.articles_arr.append(articles)
+                return self.articles_arr
+        
         def __init__(self, author, magazine, title):
             self.author = author
             self.magazine = magazine
             self.title = title
 
-        @property
+        # @property
         def author(self):
-            return self._author
+            pass
         
 
     
-    def magazines(self):
-        pass
+        def magazines(self):
+            pass
 
-    def add_article(self, magazine, title):
-        pass
+        def add_article(self, magazine, title):
+            pass
 
-    def topic_areas(self):
-        pass
+        def topic_areas(self):
+            pass
 
 class Magazine:
     def __init__(self, name, category):
@@ -116,4 +141,10 @@ class Magazine:
 
 joe = Author("Joe")
 vogue = Magazine("Vogue", "Fashion")
-article00 = (joe, vogue, "50 Ways to Not Pay Taxes")
+arttitle = "50 Ways to Not Pay Taxes"
+article00 = (joe, vogue, arttitle)
+
+moe = Author("Moe")
+wsj = Magazine("WSJ", "Business")
+arttitle2 = "100 Ways to go to Prison for Free"
+article01 = (moe, wsj, arttitle2) 
