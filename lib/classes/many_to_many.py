3ade9201970c5af0f207254c752014f4ec0935d5
rm0087 <michetti.ray@gmail.com>
2024-07-31 00:17:45 -0400

    ray branch

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,13 +1,7 @@
 class Article:
     all = []
-    test_dict = {"author":"", 
-                 "magazine":"", 
-                 "title":""}
-    
     authors = []
-    articles = []
-    magazines = []
-    
+
     def __init__(self, author, magazine, title):
         self.author = author
         self.magazine = magazine
@@ -19,10 +13,8 @@ class Article:
     
     @author.setter
     def author(self, author):
-        global authors
         if isinstance(author, Author):
             self._author = author
-            Article.test_dict["author"] = self.author.name
             if author.name not in Article.authors:
                 Article.authors.append(author.name)
             else: pass
@@ -37,8 +29,6 @@ class Article:
     def magazine(self, magazine):
         if isinstance(magazine, Magazine):
             self._magazine = magazine
-            Article.test_dict["magazine"] = magazine.name
-
         else:
             print("Must be a valid magazine")
 
@@ -50,7 +40,6 @@ class Article:
     def title(self, title):
         if isinstance(title, str) and 5 <= len(title) <= 50 and not hasattr(self, 'title'):
             self._title = title
-            Article.test_dict["title"] = title
             Article.all.append(Article.test_dict.copy())
         else:
             print("Title must be a string between 5 and 50 characters")
@@ -139,12 +128,17 @@ class Magazine:
     def contributing_authors(self):
         pass
 
-joe = Author("Joe")
-vogue = Magazine("Vogue", "Fashion")
-arttitle = "50 Ways to Not Pay Taxes"
-article00 = (joe, vogue, arttitle)
+author_1 = Author("Carry Bradshaw")
+author_2 = Author("Nathaniel Hawthorne")
+magazine = Magazine("Vogue", "Fashion")
+article_1 = Article(author_1, magazine, "How to wear a tutu with style")
+article_2 = Article(author_1, magazine, "Dating life in NYC")
+article_3 = Article(author_2, magazine, "How to be single and happy")        
 
-moe = Author("Moe")
-wsj = Magazine("WSJ", "Business")
-arttitle2 = "100 Ways to go to Prison for Free"
-article01 = (moe, wsj, arttitle2) 
+
+# assert len(author_1.articles()) == 2
+# assert len(author_2.articles()) == 1
+# assert article_1 in author_1.articles()
+# assert article_2 in author_1.articles()
+# assert article_3 not in author_1.articles()
+# assert article_3 in author_2.articles()
