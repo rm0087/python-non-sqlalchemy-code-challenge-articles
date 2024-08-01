8a8eaffb021b61ca4597646287d78a3e45117173
rm0087 <michetti.ray@gmail.com>
2024-08-01 14:17:38 -0400

    completed

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -53,7 +53,6 @@ class Author:
     
     def __init__(self, name):
         self.name = name
-        # self._articles_arr = []
 
     @property ## AUTHOR.NAME ###############################################################
     def name(self):
@@ -66,28 +65,22 @@ class Author:
         else:
             print("Author name must be a string with at least 1 character!")
     
-    # @property ## AUTHOR.ARTICLES ###############################################################
+    ## AUTHOR.ARTICLES ###############################################################
     def articles(self):
         return [article for article in Article.all if article.author == self]
     
-    
-        def __init__(self, author, magazine, title):
-            self.author = author
-            self.magazine = magazine
-            self.title = title
+    def magazines(self):
+        return list({article.magazine for article in self.articles()})
 
-        # @property
-        def author(self):
-            pass
+    def add_article(self, magazine, title):
+        return Article(self, magazine, title)
+
+    def topic_areas(self):
+        if len(self.articles()) > 0:
+            return list({article.magazine.category for article in self.articles()})
+        else:
+            return None
         
-        def magazines(self):
-            pass
-
-        def add_article(self, magazine, title):
-            pass
-
-        def topic_areas(self):
-            pass
 
 class Magazine:
     def __init__(self, name, category):
@@ -115,32 +108,69 @@ class Magazine:
         else:
             print("Magazine category must be a string that is at least 1 character")  
     
-    
     def articles(self):
-        pass
-
-                
+        return [article for article in Article.all if article.magazine == self ]
 
     def contributors(self):
-        pass
-
+        return list({article.author for article in self.articles()})
+        
     def article_titles(self):
-        pass
+        if len(self.articles()) > 0:
+            return [article.title for article in self.articles()]
+        else:
+            None
 
     def contributing_authors(self):
-        pass
+        author_count = {}
+        for article in self.articles():
+            author = article.author
+            if author in author_count:
+                author_count[author] += 1
+            else:
+                author_count[author] = 1
+        if author_count[author] >= 2:
+            return [author for author in author_count]
+        else:
+            None
 
-author_1 = Author("Carry Bradshaw")
-author_2 = Author("Nathaniel Hawthorne")
-magazine = Magazine("Vogue", "Fashion")
-article_1 = Article(author_1, magazine, "How to wear a tutu with style")
-article_2 = Article(author_1, magazine, "Dating life in NYC")
-article_3 = Article(author_2, magazine, "How to be single and happy")        
+# author_1 = Author("Carry Bradshaw")
+# author_2 = Author("Nathaniel Hawthorne")
+# magazine = Magazine("Vogue", "Fashion")
+# article_1 = Article(author_1, magazine, "How to wear a tutu with style")
+# article_2 = Article(author_1, magazine, "Dating life in NYC")
+# article_3 = Article(author_2, magazine, "How to be single and happy")        
 
 
-# assert len(author_1.articles()) == 2
-# assert len(author_2.articles()) == 1
-# assert article_1 in author_1.articles()
-# assert article_2 in author_1.articles()
-# assert article_3 not in author_1.articles()
-# assert article_3 in author_2.articles()
+# # assert len(author_1.articles()) == 2
+# # assert len(author_2.articles()) == 1
+# # assert article_1 in author_1.articles()
+# # assert article_2 in author_1.articles()
+# # assert article_3 not in author_1.articles()
+# # assert article_3 in author_2.articles()
+
+# author_1 = Author("Carry Bradshaw")
+# magazine_1 = Magazine("Vogue", "Fashion")
+# magazine_2 = Magazine("AD", "Architecture")
+# Article(author_1, magazine_1, "How to wear a tutu with style")
+# Article(author_1, magazine_2, "2023 Eccentric Design Trends")
+# Article(author_1, magazine_2, "Carrara Marble is so 2020")
+
+# author_1 = Author("Carry Bradshaw")
+# author_2 = Author("Giorgio Faletti")
+# magazine_1 = Magazine("Vogue", "Fashion")
+# magazine_2 = Magazine("AD", "Architecture")
+# author_1.add_article(magazine_1, "How to wear a tutu with style")
+# author_1.add_article(magazine_1, "Dating life in NYC")
+# author_1.add_article(magazine_2, "2023 Eccentric Design Trends")
+
+# author_1 = Author("Carry Bradshaw")
+# author_2 = Author("Nathaniel Hawthorne")
+# magazine_1 = Magazine("Vogue", "Fashion")
+# magazine_2 = Magazine("AD", "Architecture")
+# Article(author_1, magazine_1, "How to wear a tutu with style")
+# Article(author_1, magazine_1, "How to be single and happy")
+# Article(author_1, magazine_1, "Dating life in NYC")
+# Article(author_1, magazine_2, "Carrara Marble is so 2020")
+# Article(author_2, magazine_2, "2023 Eccentric Design Trends")
+
+# assert author_1 in magazine_1.contributing_authors()
